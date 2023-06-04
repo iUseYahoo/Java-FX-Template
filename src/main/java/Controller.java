8d80f0b0590ad18010e84b4caa91d7e6394da227
iUseYahoo <logicatedlzyt@gmail.com>
2023-06-04 17:48:18 +1000

    Essential tempate

---
+++ src/main/java/Controller.java
@@ -0,0 +1,10 @@
+import javafx.fxml.FXML;
+import javafx.scene.control.*;
+import javafx.event.ActionEvent;
+
+public class Controller {
+    @FXML
+    public void initialize() {
+        System.out.println("Controller started!");
+    }
+}
---
+++ src/main/java/Main.java
@@ -0,0 +1,24 @@
+import javafx.application.Application;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class Main extends Application {
+    @Override
+    public void start(Stage stage) throws Exception {
+        Parent root = FXMLLoader.load(getClass().getResource("Main.fxml"));
+
+        Scene scene = new Scene(root);
+        stage.setTitle("GUI Application title");
+        stage.setScene(scene);
+        stage.setResizable(false);
+        stage.show();
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
